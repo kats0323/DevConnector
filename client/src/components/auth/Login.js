b0e6b7c79d6144eaf8726439eacefeb7d79d1edc
Kats <katsu.0323.jpn@icloud.com>
2020-04-11 21:27:26 +0900

    done user authentication

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,10 @@
 import React, { Fragment, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import {login} from '../../actions/auth';
 
-const Login = () => {
+const Login = ({login, isAuthenticated}) => {
     const [formData, setFormData] = useState({
         email: '',
         password: ''
@@ -14,9 +17,12 @@ const Login = () => {
 
     const onSubmit = async e => {
         e.preventDefault();
-        console.log("Success");
+        login(email,password);
     }
-
+    // Redirect if logged in
+    if(isAuthenticated){
+        return <Redirect to='/dashboard' />
+    }
     return (
         <Fragment>
             <h1 className="large text-primary">Sign In</h1>
@@ -28,7 +34,7 @@ const Login = () => {
                         name="email"
                         value={email}
                         onChange={e => onChange(e)}
-                        required
+                        
                     />
                 </div>
                 <div className="form-group">
@@ -39,7 +45,7 @@ const Login = () => {
                         minLength="6"
                         value={password}
                         onChange={e => onChange(e)}
-                        required
+                      
                     />
                 </div>
                 <input type="submit" className="btn btn-primary" value="Login" />
@@ -51,4 +57,16 @@ const Login = () => {
     )
 }
 
-export default Login
+Login.propTypes = {
+    login: PropTypes.func.isRequired,
+    isAuthenticated: PropTypes.bool
+}
+
+const mapStateProps = state =>({
+    isAuthenticated: state.auth.isAuthenticated
+})
+
+export default connect(
+    mapStateProps,
+    {login}
+)(Login);
